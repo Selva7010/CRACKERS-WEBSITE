9019b8c4902e1c306315df77a3dc734eea6e6a3f
Selva7010 <selvakumar18795@gmail.com>
2026-03-11 17:27:02 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,33 +1,17 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
-// import App from "./App";// 👈 CHECK PATH
-// import "./index.css";
-// import { CartProvider } from "./context/CartContext";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <CartProvider>
-//         <App />
-//       </CartProvider>
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { CartProvider } from "./context/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <CartProvider>
         <App />
       </CartProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
